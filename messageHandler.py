f8488e1a4ced30b93182219fec69b88bcb03c11c
hacmorgan <ham430@gmail.com>
2019-12-14 17:43:31 +1100

    Added message handler

---
+++ messageHandler.py
@@ -0,0 +1,11 @@
+# Imports
+import csv
+import os
+
+# Functions for GUI use
+def getListOfMessages():
+    import os 
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    print(dir_path)
+
+getListOfMessages()
